9f0bad3c16f250798d197da0f33dbcf2b09a43d5
Juanmanato <natobily20sas@yahoo.com.ar>
2021-08-07 12:25:06 -0300

    Juanmanato
    subiendo estructura de sitio

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(3001, 
+    console.log('Servidor Funcionando'))
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+
+app.get('/', function (req, res) {
+res.sendFile(path.join(__dirname,"/views/index.html"))
+    })
