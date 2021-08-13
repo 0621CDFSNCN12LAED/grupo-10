23af4e45b77207dd505710679dd3991fad0ff1d1
ctunut <87380845+ctunut@users.noreply.github.com>
2021-08-12 21:51:11 -0300

    Agrego /carrito.  Subo 3 imágenes y modifico el app.js

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-app.listen(3001, 
-    console.log('Servidor Funcionando'))
+app.listen(3001, console.log("Servidor Funcionando"));
 
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, "/public")));
 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/views/index.html"));
+});
 
-app.get('/', function (req, res) {
-res.sendFile(path.join(__dirname,"/views/index.html"))
-    })
+app.get("/Plantas", function (req, res) {
+    res.sendFile(path.join(__dirname, "/views/productosPlantas.html"));
+});
 
-app.get('/Plantas', function (req, res) {
-res.sendFile(path.join(__dirname,"/views/productosPlantas.html"))
-})
+app.get("/Iluminacion", function (req, res) {
+    res.sendFile(path.join(__dirname, "/views/productosIluminacion.html"));
+});
+app.get("/Muebles", function (req, res) {
+    res.sendFile(path.join(__dirname, "/views/productosMuebles.html"));
+});
 
-app.get('/Iluminacion', function (req, res) {
-    res.sendFile(path.join(__dirname,"/views/productosIluminacion.html"))
-    })
-    app.get('/Muebles', function (req, res) {
-        res.sendFile(path.join(__dirname,"/views/productosMuebles.html"))
-        })
+app.get("/Carrito", function (req, res) {
+    res.sendFile(path.join(__dirname, "/views/carrito.html"));
+});
