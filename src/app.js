76757288c2561917c205ef804d6b07006bf5a1a6
ctunut <87380845+ctunut@users.noreply.github.com>
2021-08-30 20:45:37 -0300

    CTunut

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const productosController = require("./controller/productosController");
-const routerIndex = require("./controller/indexController")
+const routerIndex = require("./controller/indexController");
+const carritoController = require("./controller/carritoController");
 
-app.set ("view engine", "ejs")
+app.set("view engine", "ejs");
 
 app.listen(3001, console.log("Servidor Funcionando"));
 
 app.use(express.static(path.join(__dirname, "../public")));
-
 
 app.use("/", routerIndex.index);
 
@@ -17,13 +17,11 @@ app.get("/Login", function (req, res) {
     res.sendFile(path.join(__dirname, "/views/login.html"));
 });
 
-app.get('/registro', function (req, res) {
-    res.sendFile(path.join(__dirname,"/views/registro.html"))
-    })
-
-app.get("/Carrito", function (req, res) {
-    res.sendFile(path.join(__dirname, "/views/carrito.html"));
+app.get("/registro", function (req, res) {
+    res.sendFile(path.join(__dirname, "/views/registro.html"));
 });
+
+app.use("/Carrito", carritoController.carrito);
 
 app.get("/Plantas", function (req, res) {
     res.sendFile(path.join(__dirname, "/views/productosPlantas.html"));
@@ -32,5 +30,4 @@ app.get("/Plantas", function (req, res) {
 app.get("/Iluminacion", function (req, res) {
     res.sendFile(path.join(__dirname, "/views/productosIluminacion.html"));
 });
-app.use("/Muebles", productosController.listadoProductos)
-
+app.use("/Muebles", productosController.listadoProductos);
